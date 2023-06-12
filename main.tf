b912f509c2305ae2d35fe8f947eebfd9ac36f5f3
abdyusma <111057405+abdyusma@users.noreply.github.com>
2023-06-13 03:57:44 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "pandai"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "pandai"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-sn"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.0.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforMySQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+
+resource "azurerm_private_dns_zone" "example" {
+  name                = "pandai.private.mysql.database.azure.com"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "example" {
+  name                  = "pandai.private.mysql.database.azure.com/d5vb7pibvforq"
+  private_dns_zone_name = azurerm_private_dns_zone.example.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+  resource_group_name   = azurerm_resource_group.example.name
+}
+
+resource "azurerm_mysql_flexible_server" "example" {
+  name                   = "pandai"
+  resource_group_name    = azurerm_resource_group.example.name
+  location               = azurerm_resource_group.example.location
+  administrator_login    = "psqladmin"
+  administrator_password = "H@Sh1CoR3!"
+  backup_retention_days  = 7
+  delegated_subnet_id    = azurerm_subnet.example.id
+  private_dns_zone_id    = azurerm_private_dns_zone.example.id
+  sku_name               = "GP_Standard_D2ds_v4"
+
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.example]
+}
